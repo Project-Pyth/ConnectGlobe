cdc6c7a8d3e81df8525d012c5986b7e0399f7a04
Project-Pyth <65401018+Project-Pyth@users.noreply.github.com>
2020-07-01 14:10:23 +0400

    add favrourite post field

--- ConnectGlobe/Globe/models.py
+++ ConnectGlobe/Globe/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 from django.urls import reverse
 # Create your models here.
 from django.utils.timezone import now
+from tinymce import models as tinymce
 
 
 class Feedback(models.Model):
@@ -38,7 +39,7 @@ class MyProfile(models.Model):
     city = models.CharField(max_length=100, blank=True, null=True)
     state = models.CharField(max_length=100, blank=True, null=True)
     bio = models.TextField(max_length=150, blank=True, null=True)
-    image = models.ImageField(upload_to='profile_pics', null=True)
+    image = models.ImageField(upload_to='profile_pics', blank=True, null=True)
 
 
 @receiver(post_save, sender=User)
@@ -56,22 +57,24 @@ class Post(models.Model):
         ("Java", "Java"),
         ("C++", "C++"),
         ("C", "C"),
+        ("HTML", "HTML"),
+        ("CSS", "CSS"),
+        ("Bootstrap", "Bootstrap"),
+        ("HTML/CSS/Bootstrap-Coding", "HTML/CSS/Bootstrap-Coding"),
         ("Database", "Database"),
     )
     name = models.ForeignKey(User, on_delete=models.PROTECT, related_name='name')
-    email = models.EmailField(max_length=100)
     title = models.CharField(max_length=100, blank=False, null=False)
     category = models.CharField(choices=CATEGORY_CHOICES, default='Django', max_length=100, blank=False, null=False)
-    content = models.TextField(blank=False, null=False)
+    content = tinymce.HTMLField(blank=False, null=False)
     file = models.FileField(upload_to='project_files', null=True, blank=True)
     slug = models.CharField(max_length=130)
     date_posted = models.DateTimeField(default=timezone.now)
+    poststatus = models.BooleanField(default=False)
+    favourite = models.ManyToManyField(User, related_name='favourite', blank=True)
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk':self.pk})
 
 
 class PostComment(models.Model):
